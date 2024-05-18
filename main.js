2254ac9036dacafb1c917825d0467c8e06e23ab1
David69699 <abutidzedato18@gmail.com>
2024-05-18 23:51:08 +0400

    feat - created data.json

--- main.js
+++ main.js
@@ -2,33 +2,7 @@ const express = require("express");
 const apiRouter = require("./api/apiRoute");
 const app = express();
 const PORT = 3000;
-
-const data = [
-  {
-    id: 2,
-    name: "Samsung",
-    price: "1200",
-    created: "2024-05-18T08:27:18.453Z",
-  },
-  {
-    id: 3,
-    name: "cigarette",
-    price: "25",
-    created: "2024-05-18T08:38:55.969Z",
-  },
-  {
-    id: 4,
-    name: "donaldduck",
-    price: "50",
-    created: "2024-05-18T08:39:01.237Z",
-  },
-  {
-    id: 5,
-    name: "bartyi",
-    price: "900",
-    created: "2024-05-18T12:22:22.894Z",
-  },
-];
+const data = require("./data.json");
 
 app.use("/api", apiRouter);
 
